a5c3dd885a64f886fc94891e801c0f18e413a72a
adam <adam000202@gmail.com>
2020-02-04 22:30:55 +0100

    petit erreur sur le posY dans Bordeer, ajout d'animation qui tombe entre trois bordures

--- src/main/java/flipper/Border.java
+++ src/main/java/flipper/Border.java
@@ -1,5 +1,4 @@
 public class Border{//une bordure est considerer comme une ligne
-  private double distance;
   private Position posX;
   public Position getPosX(){
     return posX;
@@ -8,15 +7,16 @@ public class Border{//une bordure est considerer comme une ligne
   public Position getPosY(){
     return posY;
   }
+  private double distance;
   private double rebond;//contient une variable de rebond entre 0 et 1
-  public Border(Position posX,Position poxY,double rebond){
+  public Border(Position posX,Position posY,double rebond){
     this.posX=posX;
     this.posY=posY;
-    this.distance=posX.distance(posY);
+    this.distance=this.posX.distance(this.posY);
     if(rebond>=0&&rebond<=1)this.rebond=rebond;
     else this.rebond=0;
   }
   public boolean isOnTheLine(Position pos){
     return pos.distance(this.posX)+pos.distance(this.posY)==this.distance;
-  }  
+  }
 }
--- src/main/java/flipper/TestGraphique.java
+++ src/main/java/flipper/TestGraphique.java
@@ -12,14 +12,24 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.animation.TranslateTransition;
 import javafx.util.Duration;
+import javafx.scene.layout.Pane;
 public class TestGraphique extends Application{
+  Borders border;
   public static void main(String[] args) {
     launch(args);
   }
   public void start(Stage primaryStage){
+    border=new Borders();
+    border.addBorder(new Border(new Position(20,800),new Position(580,800),1));
+    border.addBorder(new Border(new Position(20,200),new Position(20,800),1));
+    border.addBorder(new Border(new Position(580,200),new Position(580,800),1));
     Circle circle=new Circle(200,200,100);
+    Pane pane=new Pane();
+    pane.getChildren().add(circle);
+    for(Border b:border.getBorders()){
+      pane.getChildren().add(new Line(b.getPosX().getX(),b.getPosX().getY(),b.getPosY().getX(),b.getPosY().getY()));
+    }
     //Adding all the elements to the path
-    Group root=new Group(circle);
     TranslateTransition translateTransition=new TranslateTransition();
     translateTransition.setDuration(Duration.millis(1020));
     translateTransition.setNode(circle);
@@ -27,7 +37,7 @@ public class TestGraphique extends Application{
     translateTransition.setCycleCount(50);
     translateTransition.setAutoReverse(false);
     translateTransition.play();
-    Scene scene=new Scene(root,600,900);
+    Scene scene=new Scene(pane,600,900);
     scene.setFill(Color.BROWN);
     primaryStage.setScene(scene);
     primaryStage.setTitle("TestGraphique");
