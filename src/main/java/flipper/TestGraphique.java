5593ba02508075445859f0e9f7662e020b17bd3f
Tanel Oubadia <tobitreex@gmail.com>
2020-03-20 19:23:50 +0100

    rajout d'images en plus

--- src/main/java/flipper/TestGraphique.java
+++ src/main/java/flipper/TestGraphique.java
@@ -29,7 +29,7 @@ public class TestGraphique extends Application{
     launch(args);
   }
   public void start(Stage primaryStage){
-    Quadrilatere q=new Quadrilatere(0.5,new Position(110,640),new Position(100,540),new Position(200,700),new Position(200,700));
+    Quadrilatere q=new Quadrilatere(0.5,new Position(110,640),new Position(110,540),new Position(200,700),new Position(200,700));
     RandomShape r=new RandomShape(q);
 
     r.addCircle(15,2);
@@ -54,7 +54,7 @@ public class TestGraphique extends Application{
     p2.setFill(Color.GREEN);
     p2.getPoints().addAll(q2.getAllPosition());
 
-    Quadrilatere q3=new Quadrilatere(0.5,new Position(100,300),new Position(150,300),new Position(150,400),new Position(100,400));
+    Quadrilatere q3=new Quadrilatere(0.5,new Position(100,300),new Position(139,300),new Position(139,450),new Position(100,450));
     RandomShape r3=new RandomShape(q3);
     r3.addCircle(30,1);
     r3.addCircle(50,2);
@@ -66,7 +66,7 @@ public class TestGraphique extends Application{
     p3.setFill(Color.GREEN);
     p3.getPoints().addAll(q3.getAllPosition());
 
-    Quadrilatere q4=new Quadrilatere(0.5,new Position(200,300),new Position(250,300),new Position(250,400),new Position(200,400));
+    Quadrilatere q4=new Quadrilatere(0.5,new Position(200,300),new Position(239,300),new Position(239,450),new Position(200,450));
     RandomShape r4=new RandomShape(q4);
     r4.addCircle(30,1);
     r4.addCircle(50,2);
@@ -78,7 +78,7 @@ public class TestGraphique extends Application{
     p4.setFill(Color.GREEN);
     p4.getPoints().addAll(q4.getAllPosition());
 
-    Quadrilatere q5=new Quadrilatere(0.5,new Position(350,300),new Position(400,300),new Position(400,400),new Position(350,400));
+    Quadrilatere q5=new Quadrilatere(0.5,new Position(350,300),new Position(389,300),new Position(389,450),new Position(350,450));
     RandomShape r5=new RandomShape(q5);
     r5.addCircle(30,1);
     r5.addCircle(50,2);
@@ -90,7 +90,7 @@ public class TestGraphique extends Application{
     p5.setFill(Color.GREEN);
     p5.getPoints().addAll(q5.getAllPosition());
 
-    Quadrilatere q6=new Quadrilatere(0.5,new Position(450,300),new Position(500,300),new Position(500,400),new Position(450,400));
+    Quadrilatere q6=new Quadrilatere(0.5,new Position(450,300),new Position(489,300),new Position(489,450),new Position(450,450));
     RandomShape r6=new RandomShape(q6);
     r6.addCircle(30,1);
     r6.addCircle(50,2);
@@ -128,9 +128,9 @@ public class TestGraphique extends Application{
 
     Balle balle=new Balle(new Position(100,200),10,5);
     border=new Borders();
-    border.addBorder(new Border(new Position(490,540),new Position(390,700),0.9));
-    border.addBorder(new Border(new Position(480,640),new Position(490,540),0.9));
-    border.addBorder(new Border(new Position(480,640),new Position(390,700),0.9));
+    border.addBorder(new Border(new Position(480,540),new Position(390,690),0.9));
+    border.addBorder(new Border(new Position(480,660),new Position(480,540),0.9));
+    border.addBorder(new Border(new Position(480,660),new Position(390,690),0.9));
     border.addBorder(new Border(new Position(20,680),new Position(220,750),0.9));
     border.addBorder(new Border(new Position(550,680),new Position(350,750),0.9));
     border.addBorder(new Border(new Position(20,815),new Position(550,845),0.9));
@@ -141,14 +141,53 @@ public class TestGraphique extends Application{
     border.addBorder(new Border(new Position(0,850),new Position(590,850),0.9));
     Circle circle=new Circle(balle.getPos().getX(),balle.getPos().getY(),balle.getR());
 
-    Image image = new Image("file:rectangle_arrondi.png");
+    Image rect = new Image("file:rectangle_arrondi.png");
+    Image triangle = new Image("file:triangle_bas.png");
+    Image triangle2 = new Image("file:triangle_bas2.png");
+    Image rond = new Image("file:rond.png");
+
     ImageView iv = new ImageView();
-    iv.setImage(image);
-    iv.setRotate(90);
+    iv.setImage(rect);
     iv.setPreserveRatio(true);
-    iv.setFitWidth(150);
-    iv.setX(50);
-    iv.setY(300);
+    iv.setFitHeight(150);
+    iv.setX(100);iv.setY(300);
+
+    ImageView iv2 = new ImageView();
+    iv2.setImage(rect);
+    iv2.setPreserveRatio(true);
+    iv2.setFitHeight(150);
+    iv2.setX(200);iv2.setY(300);
+
+    ImageView iv3 = new ImageView();
+    iv3.setImage(rect);
+    iv3.setPreserveRatio(true);
+    iv3.setFitHeight(150);
+    iv3.setX(350);iv3.setY(300);
+
+    ImageView iv4 = new ImageView();
+    iv4.setImage(rect);
+    iv4.setPreserveRatio(true);
+    iv4.setFitHeight(150);
+    iv4.setX(450);iv4.setY(300);
+
+    ImageView iv5 = new ImageView();
+    iv5.setImage(triangle);
+    iv5.setPreserveRatio(true);
+    iv5.setX(110);iv5.setY(540);
+
+    ImageView iv6 = new ImageView();
+    iv6.setImage(triangle2);
+    iv6.setPreserveRatio(true);
+    iv6.setFitHeight(150);
+    iv6.setX(390);iv6.setY(540);
+
+    ImageView iv7 = new ImageView();
+    iv7.setImage(rond);
+    iv7.setX(100);iv7.setY(50);
+
+    ImageView iv8 = new ImageView();
+    iv8.setImage(rond);
+    iv8.setX(400);iv8.setY(50);
 
     Pane pane=new Pane();
     pane.getChildren().add(circle);
@@ -160,9 +199,16 @@ public class TestGraphique extends Application{
     pane.getChildren().add(p5); //rectangle 3
     pane.getChildren().add(p6); //rectangle 4
     pane.getChildren().add(p7); //losange 1(qui est censé etre un rond)
-    pane.getChildren().add(p8); //losange 2
+    pane.getChildren().add(p8); //losange
+    pane.getChildren().add(iv); //image rect1
+    pane.getChildren().add(iv2);
+    pane.getChildren().add(iv3);
+    pane.getChildren().add(iv4);
+    pane.getChildren().add(iv5); //image triangle bas
+    pane.getChildren().add(iv6);
+    pane.getChildren().add(iv7);
+    pane.getChildren().add(iv8);
 
-    pane.getChildren().add(iv);
     for(Border b:border.getBorders()){
       pane.getChildren().add(new Line(b.getPosX().getX(),b.getPosX().getY(),b.getPosY().getX(),b.getPosY().getY()));
     }
